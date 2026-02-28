9bd18631785e2ca3d37d3d2cb6c600a60b78ab28
hareesh reddy <hareeshreddy916@gmail.com>
2026-02-28 16:10:42 +0530

    Day 2: Added Student model and Get /students API

--- src/main/java/com/hareesh/career/controller/StudentController.java
+++ src/main/java/com/hareesh/career/controller/StudentController.java
@@ -1,7 +1,11 @@
 package com.hareesh.career.controller;
 
+import com.hareesh.career.model.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -9,5 +13,13 @@ public class StudentController {
     @GetMapping("/health")
     public String health() {
         return "Backend Running Successfully!";
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return Arrays.asList(
+                new Student(1, "Hareesh", "CS"),
+                new Student(2, "Rahul", "IT")
+        );
     }
 }
