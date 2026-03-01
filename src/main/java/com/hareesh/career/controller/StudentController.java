ded027d2644fb8e477897b81e9ff7667975cbf4a
hareesh reddy <hareeshreddy916@gmail.com>
2026-03-01 21:33:07 +0530

    Day 3: Added Post /students endpoint with RequestBody handling

--- src/main/java/com/hareesh/career/controller/StudentController.java
+++ src/main/java/com/hareesh/career/controller/StudentController.java
@@ -1,14 +1,18 @@
 package com.hareesh.career.controller;
 
 import com.hareesh.career.model.Student;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
-
+import java.util.ArrayList;
 @RestController
 public class StudentController {
+	
+private List<Student> dynamicstudents = new ArrayList<>();
 
     @GetMapping("/health")
     public String health() {
@@ -21,5 +25,16 @@ public class StudentController {
                 new Student(1, "Hareesh", "CS"),
                 new Student(2, "Rahul", "IT")
         );
+    }
+    
+    @PostMapping("/students/new")
+    public String addDynamicStudent(@RequestBody Student student) {
+    	dynamicstudents.add(student);
+    	return "Dynamic Student Added Successfully!";
+    }
+    
+    @GetMapping("/students/new")
+    public List<Student> getDynamicStudents(){
+    	return dynamicstudents;
     }
 }
